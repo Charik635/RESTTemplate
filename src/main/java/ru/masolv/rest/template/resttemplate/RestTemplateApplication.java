1c92040563cf7a3b4bb2bee001a4f4cef3dfccf4
Charik635 <cuber1319@gmail.com>
2021-08-02 13:03:05 +0300

    Ready

--- src/main/java/ru/masolv/rest/template/resttemplate/RestTemplateApplication.java
+++ src/main/java/ru/masolv/rest/template/resttemplate/RestTemplateApplication.java
@@ -2,6 +2,8 @@ package ru.masolv.rest.template.resttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class RestTemplateApplication {
@@ -9,5 +11,6 @@ public class RestTemplateApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestTemplateApplication.class, args);
     }
+
 
 }
